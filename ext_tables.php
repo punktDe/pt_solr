ad302fa1810f44ef45c4486b1476683dc7a03e34
Michael Knoll <mimi@kaktusteam.de>
2012-02-10 01:49:53 +0100

    TASK: Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Michael Knoll <knoll@punkt.de>, punkt.de GmbH
+*
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+// Register plugin in available plugins
+Tx_Extbase_Utility_Extension::registerPlugin(
+	$_EXTKEY,
+	'Pi1',
+	'Solr (punkt.de)'
+);
+
+//$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+//t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+
+// Make static TypoScript template available in includes
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[pt_solr] Solr');
+
+?>
