dbe63718ea4d0eb2756b2f9ea2d820310722f4d2
Michael Knoll <mimi@kaktusteam.de>
2012-02-17 04:37:38 +0100

    ADD: solr list identifier is now taken from FlexForm

--- Classes/Controller/AbstractActionController.php
+++ Classes/Controller/AbstractActionController.php
@@ -32,5 +32,45 @@
  */
 class Tx_PtSolr_Controller_AbstractActionController extends Tx_PtExtbase_Controller_AbstractActionController {
 
+    /**
+     * Holds list identifier of solr list configuration set in TS or FlexForm
+     *
+     * @var string
+     */
+    protected $solrExtlistIdentifier;
+
+
+
+    /**
+     * Holds extlist context for given solr configuration
+     *
+     * @var Tx_PtExtlist_ExtlistContext_ExtlistContext
+     */
+    protected $solrExtlistContext;
+
+
+
+    /**
+     * Initializes controller before invoking an action
+     */
+    protected function initializeAction() {
+        parent::initializeAction();
+        $this->determineSolrListIdentifier();
+    }
+
+
+
+    /**
+     * Sets solr extlist identifier from flexform or uses 'solr' as default value
+     */
+    protected function determineSolrListIdentifier() {
+        if (isset($this->settings['listIdentifier']) && $this->settings['listIdentifier'] !== '') {
+            $listIdentifier = $this->settings['listIdentifier'];
+        } else {
+            $listIdentifier = 'solr';
+        }
+        $this->solrExtlistIdentifier = $listIdentifier;
+    }
+
 }
 ?>
